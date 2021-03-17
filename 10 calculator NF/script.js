0ac7026d083ef4277e89001a36fd5937682050bb
DenisKirpichnik <68986820+DenisKirpichnik@users.noreply.github.com>
2021-03-18 00:08:34 +0300

    my homework

--- 10 calculator NF/script.js
+++ 10 calculator NF/script.js
@@ -1,154 +1,145 @@
 let buffer = document.querySelector(".buffer__display");
-let currentV = document.querySelector(".currentValue__display");
+let currentV = document.querySelector(".currentValue__display"); //current operand
 let numberBtns = document.querySelectorAll(".btn__numbers ");
 let operatorsBtns = document.querySelectorAll(".btn__operators ");
 
-// previous and current operands
-let previousO = null
-let currentO = currentV.innerText
-// setting the operator
-let operator = "divide"
+// array for calculation
+let bufferArr = [];
+buffer.innerText = bufferArr.join("");
+let currentValue = null;
 
-//checking if the last character of buffer is an operator 
-let lastChar = null
-// This function sets last char to the last char of buffer by default
-function setLastChar(n = buffer.innerText.charAt(buffer.innerText.length - 1)) {
-    lastChar = n
-}
-
-// operator check arr, last char == operator?, blocks adding new to buffer
-let arr = ["+", "-", "/", "*"]
-
-// number buttons functionality 
+// number buttons functionality
 for (let i = 0; i < numberBtns.length; i++) {
-    numberBtns[i].addEventListener("click", (e) => {
-        currentV.innerText += e.target.value
-    })
+  numberBtns[i].addEventListener("click", (e) => {
+    currentV.innerText += e.target.value;
+  });
+}
+// pushes current value and the operator to the array, sets current val to null, displays buffer
+function pushCurrAndTargetVupdateDisplay(targetValue) {
+  bufferArr.push(currentV.innerText);
+  bufferArr.push(targetValue);
+  currentV.innerText = null;
+  buffer.innerText = bufferArr.join("");
 }
 
-//handlesequence function
-function handleSequence(e) {
-    console.log("prev-", previousO, "curr-", currentO)
-    buffer.innerText = buffer.innerText + currentV.innerText + e.target.value
-
-    currentV.innerText = Number(previousO) + Number(currentV.innerText)
-    previousO = currentV.innerText
-    console.log("handleSequence was called oops")
-    setLastChar()
-
-}
-//operators buttons functionality 
+//operators buttons functionality
 function handleOperation(e) {
-    if (arr.includes(lastChar) && currentV.innerText.length >= 1 && e.target.value === "+" || e.target.value === "-" || e.target.value === "/" || e.target.value === "*") {
-        handleSequence(e)
-    } else if (e.target.value === "+") {
-        operator = "add"
-        previousO = currentV.innerText
-        currentV.innerText = null
-        buffer.innerText = buffer.innerText + previousO + e.target.value
-        setLastChar(e.target.value)
-
-    } else if (e.target.value === "-") {
-        operator = "subtract"
-        previousO = currentV.innerText
-        currentV.innerText = null
-        buffer.innerText = buffer.innerText + previousO + e.target.value
-        setLastChar(e.target.value)
-    } else if (e.target.value === "/") {
-        operator = "divide"
-        previousO = currentV.innerText
-        currentV.innerText = null
-        buffer.innerText = buffer.innerText + previousO + e.target.value
-        setLastChar(e.target.value)
-    } else if (e.target.value === "*") {
-        operator = "multiply"
-        previousO = currentV.innerText
-        currentV.innerText = null
-        buffer.innerText = buffer.innerText + previousO + e.target.value
-        setLastChar(e.target.value)
-    }
+  if (e.target.value === "+") {
+    pushCurrAndTargetVupdateDisplay(e.target.value);
+  } else if (e.target.value === "-") {
+    pushCurrAndTargetVupdateDisplay(e.target.value);
+  } else if (e.target.value === "/") {
+    pushCurrAndTargetVupdateDisplay(e.target.value);
+  } else if (e.target.value === "*") {
+    pushCurrAndTargetVupdateDisplay(e.target.value);
+  }
 }
 
 for (let i = 0; i < operatorsBtns.length; i++) {
-    operatorsBtns[i].addEventListener("click", handleOperation)
+  operatorsBtns[i].addEventListener("click", handleOperation);
 }
 
 //equal functionality
 function handleEqual(e) {
-    e.preventDefault()
-    switch (operator) {
-        case "add":
-            console.log(lastChar)
-            currentO = currentV.innerText
-            currentV.innerText = Number(previousO) + Number(currentO)
-            buffer.innerText = null
-            setLastChar(null)
-            break;
-        case "subtract":
-            currentO = currentV.innerText
-            currentV.innerText = previousO - currentO
-            buffer.innerText = null
-            setLastChar(null)
-            break;
-        case "multiply":
-            currentO = currentV.innerText
-            currentV.innerText = previousO * currentO
-            buffer.innerText = null
-            setLastChar(null)
-            break;
-        case "divide":
-            currentO = currentV.innerText
-            currentV.innerText = previousO / currentO
-            buffer.innerText = null
-            setLastChar(null)
-            break;
-
-    }
+  e.preventDefault();
+  if (currentV.innerText.length > 0) {
+    bufferArr.push(currentV.innerText);
+  }
+  let lastChar = bufferArr[bufferArr.length - 1];
+  if (
+    lastChar === "+" ||
+    lastChar === "-" ||
+    lastChar === "*" ||
+    lastChar === "/"
+  ) {
+    bufferArr.splice(-1, 1);
+  }
+  currentV.innerText = calcAddSubtract(calcMultiplyDivide(bufferArr));
+  bufferArr = [];
+  buffer.innerText = null;
 }
 
 let equalBtn = document.getElementById("equal__op");
-equalBtn.addEventListener("click", handleEqual)
+equalBtn.addEventListener("click", handleEqual);
 
-//delete one digit functionality //F 
+//delete one digit functionality //F
 deleteone_op.addEventListener("click", (e) => {
-    e.preventDefault()
-    if (currentV.innerText.length !== 0) {
-        currentV.innerText = currentV.innerText.substring(0, currentV.innerText.length - 1)
-    }
-})
+  e.preventDefault();
+  if (currentV.innerText.length !== 0) {
+    currentV.innerText = currentV.innerText.substring(
+      0,
+      currentV.innerText.length - 1
+    );
+  }
+});
 
 // clear all CE functionality //F
 clearAll__op.addEventListener("click", (e) => {
-    e.preventDefault()
-    buffer.innerText = null
-    currentV.innerText = null
-})
+  e.preventDefault();
+  bufferArr = [];
+  currentV.innerText = null;
+  buffer.innerText = null;
+});
 //clear current input functionality C //F
 clearInput__op.addEventListener("click", (e) => {
-    e.preventDefault()
-    currentV.innerText = null
-})
-
+  e.preventDefault();
+  currentV.innerText = null;
+});
 
 // negate the number functionality //F
-let negate = document.getElementById("negateCurrOperand__op")
+let negate = document.getElementById("negateCurrOperand__op");
 negate.addEventListener("click", (e) => {
-    e.preventDefault()
-    if (+currentV.innerText > 0) {
-        currentV.innerText = -Math.abs(+currentV.innerText)
-        currentV.innerText = currentV.innerText.toString()
-    } else {
-        currentV.innerText = Math.abs(+currentV.innerText)
-        currentV.innerText = currentV.innerText.toString()
-    }
-})
+  e.preventDefault();
+  if (+currentV.innerText > 0 && currentV.innerText.length > 0) {
+    currentV.innerText = -Math.abs(+currentV.innerText);
+    currentV.innerText = currentV.innerText.toString();
+  } else if (+currentV.innerText < 0 && currentV.innerText.length > 0) {
+    currentV.innerText = Math.abs(+currentV.innerText);
+    currentV.innerText = currentV.innerText.toString();
+  }
+});
 
 //decimal functionality //F
 decimal__op.addEventListener("click", (e) => {
-    e.preventDefault()
-    if (/\./.test(currentV.innerText) || currentV.innerText.length == 0) {
-        return
-    } else {
-        currentV.innerText += "."
+  e.preventDefault();
+  if (/\./.test(currentV.innerText) || currentV.innerText.length == 0) {
+    return;
+  } else {
+    currentV.innerText += ".";
+  }
+});
+
+//function recursively calculates  + || -
+function calcAddSubtract(array) {
+  let arr = array;
+  if (arr.some((e) => /[+-]/.test(e)) === false || arr.length == 1) {
+    return array;
+  }
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] == "+") {
+      let res = Number(arr[i - 1]) + Number(arr[i + 1]);
+      arr.splice(i - 1, 3, res);
+    } else if (arr[i] == "-") {
+      let res = Number(arr[i - 1]) - Number(arr[i + 1]);
+      arr.splice(i - 1, 3, res);
     }
-})
+  }
+  return calcAddSubtract(arr);
+}
+// function recursively calculates  * || /
+function calcMultiplyDivide(array) {
+  let arr = array;
+  if (arr.some((e) => /[/*]/.test(e)) === false) {
+    return array;
+  }
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] == "/") {
+      let res = (Number(arr[i - 1]) / Number(arr[i + 1])).toFixed(8);
+      arr.splice(i - 1, 3, res);
+    } else if (arr[i] == "*") {
+      let res = Number(arr[i - 1]) * Number(arr[i + 1]);
+      arr.splice(i - 1, 3, res);
+    }
+  }
+  return calcMultiplyDivide(arr);
+}
---
+++ 10 calculator NF/stuffIdontNeed.js
@@ -0,0 +1,190 @@
+//test
+function mat(str) {
+  var operators = "/*-+";
+  for (var i = 0; i < 4; i++) {
+    var op = operators.charAt(i);
+    var pos = str.indexOf(op);
+    if (pos > 0 && operators.indexOf(str.charAt(pos - 1)) == -1) {
+      var left = mat(str.substr(0, pos));
+      var right = mat(str.substr(pos + 1));
+      switch (op) {
+        case "/":
+          return left / right;
+        case "*":
+          return left * right;
+        case "-":
+          return left - right;
+        case "+":
+          return left + right;
+      }
+    }
+  }
+  return Number(str);
+}
+
+let array = [50, "/", 10];
+let join = array.join("");
+console.log(mat(50));
+
+/////////////////////////////////////// my old way
+
+let buffer = document.querySelector(".buffer__display");
+let currentV = document.querySelector(".currentValue__display");
+let numberBtns = document.querySelectorAll(".btn__numbers ");
+let operatorsBtns = document.querySelectorAll(".btn__operators ");
+
+// previous and current operands
+let previousO = null;
+let currentO = currentV.innerText;
+// setting the operator
+let operator = "divide";
+
+//checking if the last character of buffer is an operator
+let lastChar = null;
+// This function sets last char to the last char of buffer by default
+function setLastChar(n = buffer.innerText.charAt(buffer.innerText.length - 1)) {
+  lastChar = n;
+}
+
+// operator check arr, last char == operator?, blocks adding new to buffer
+let arr = ["+", "-", "/", "*"];
+
+// number buttons functionality
+for (let i = 0; i < numberBtns.length; i++) {
+  numberBtns[i].addEventListener("click", (e) => {
+    currentV.innerText += e.target.value;
+  });
+}
+
+//handlesequence function
+function handleSequence(e) {
+  console.log("prev-", previousO, "curr-", currentO);
+  buffer.innerText = buffer.innerText + currentV.innerText + e.target.value;
+
+  currentV.innerText = Number(previousO) + Number(currentV.innerText);
+  previousO = currentV.innerText;
+  console.log("handleSequence was called oops");
+  setLastChar();
+}
+//operators buttons functionality
+function handleOperation(e) {
+  if (
+    (arr.includes(lastChar) &&
+      currentV.innerText.length >= 1 &&
+      e.target.value === "+") ||
+    e.target.value === "-" ||
+    e.target.value === "/" ||
+    e.target.value === "*"
+  ) {
+    handleSequence(e);
+  } else if (e.target.value === "+") {
+    operator = "add";
+    previousO = currentV.innerText;
+    currentV.innerText = null;
+    buffer.innerText = buffer.innerText + previousO + e.target.value;
+    setLastChar(e.target.value);
+  } else if (e.target.value === "-") {
+    operator = "subtract";
+    previousO = currentV.innerText;
+    currentV.innerText = null;
+    buffer.innerText = buffer.innerText + previousO + e.target.value;
+    setLastChar(e.target.value);
+  } else if (e.target.value === "/") {
+    operator = "divide";
+    previousO = currentV.innerText;
+    currentV.innerText = null;
+    buffer.innerText = buffer.innerText + previousO + e.target.value;
+    setLastChar(e.target.value);
+  } else if (e.target.value === "*") {
+    operator = "multiply";
+    previousO = currentV.innerText;
+    currentV.innerText = null;
+    buffer.innerText = buffer.innerText + previousO + e.target.value;
+    setLastChar(e.target.value);
+  }
+}
+
+for (let i = 0; i < operatorsBtns.length; i++) {
+  operatorsBtns[i].addEventListener("click", handleOperation);
+}
+
+//equal functionality
+function handleEqual(e) {
+  e.preventDefault();
+  switch (operator) {
+    case "add":
+      console.log(lastChar);
+      currentO = currentV.innerText;
+      currentV.innerText = Number(previousO) + Number(currentO);
+      buffer.innerText = null;
+      setLastChar(null);
+      break;
+    case "subtract":
+      currentO = currentV.innerText;
+      currentV.innerText = previousO - currentO;
+      buffer.innerText = null;
+      setLastChar(null);
+      break;
+    case "multiply":
+      currentO = currentV.innerText;
+      currentV.innerText = previousO * currentO;
+      buffer.innerText = null;
+      setLastChar(null);
+      break;
+    case "divide":
+      currentO = currentV.innerText;
+      currentV.innerText = previousO / currentO;
+      buffer.innerText = null;
+      setLastChar(null);
+      break;
+  }
+}
+
+let equalBtn = document.getElementById("equal__op");
+equalBtn.addEventListener("click", handleEqual);
+
+//delete one digit functionality //F
+deleteone_op.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (currentV.innerText.length !== 0) {
+    currentV.innerText = currentV.innerText.substring(
+      0,
+      currentV.innerText.length - 1
+    );
+  }
+});
+
+// clear all CE functionality //F
+clearAll__op.addEventListener("click", (e) => {
+  e.preventDefault();
+  buffer.innerText = null;
+  currentV.innerText = null;
+});
+//clear current input functionality C //F
+clearInput__op.addEventListener("click", (e) => {
+  e.preventDefault();
+  currentV.innerText = null;
+});
+
+// negate the number functionality //F
+let negate = document.getElementById("negateCurrOperand__op");
+negate.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (+currentV.innerText > 0) {
+    currentV.innerText = -Math.abs(+currentV.innerText);
+    currentV.innerText = currentV.innerText.toString();
+  } else {
+    currentV.innerText = Math.abs(+currentV.innerText);
+    currentV.innerText = currentV.innerText.toString();
+  }
+});
+
+//decimal functionality //F
+decimal__op.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (/\./.test(currentV.innerText) || currentV.innerText.length == 0) {
+    return;
+  } else {
+    currentV.innerText += ".";
+  }
+});
